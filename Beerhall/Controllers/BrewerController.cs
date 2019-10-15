bef3701b09e3f7d31ceddbdef869a7efeb356f82
Stefaan Samyn <stefaan.samyn@hogent.be>
2019-10-15 17:37:34 +0200

    Add functionality Brewer - Index

---
+++ Beerhall/Controllers/BrewerController.cs
@@ -0,0 +1,20 @@
+﻿using Beerhall.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Beerhall.Controllers {
+    public class BrewerController : Controller {
+        private readonly IBrewerRepository _brewerRepository;
+
+        public BrewerController(IBrewerRepository brewerRepository) {
+            _brewerRepository = brewerRepository;
+        }
+
+        public IActionResult Index() {
+            IEnumerable<Brewer> brewers = _brewerRepository.GetAll().OrderBy(b => b.Name).ToList();
+            ViewData["TotalTurnover"] = brewers.Sum(b => b.Turnover);
+            return View(brewers);
+        }
+    }
+}
